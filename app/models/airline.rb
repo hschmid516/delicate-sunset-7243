94092362a63ed66bc973b4c8011c7132b97b04cf
Henry Schmid <henry.schmid1@gmail.com>
2021-09-24 10:35:07 -0600

    Add sorted passengers to view, add model test

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,8 +1,14 @@
 class Airline < ApplicationRecord
   has_many :flights
   has_many :passengers, through: :flights
+  # has_many :trips, through: :flights
 
-  def adult_passengers
-    passengers.where('age > 17').distinct
+  def sorted_adult_passengers
+    flights.joins(:trips, :passengers)
+           .select('passengers.*, COUNT(trips) as flight_count')
+           .where('passengers.id = trips.passenger_id')
+           .where('age > 17')
+           .group('passengers.id')
+           .order(flight_count: :desc)
   end
 end
