4c6cf30c4575efc7e09d811443aa3f7cfa86516d
Henry Schmid <henry.schmid1@gmail.com>
2021-09-24 09:13:33 -0600

    Add tests for airline to passengers association

--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Passenger do
   it { should have_many(:trips) }
   it { should have_many(:flights).through(:trips) }
+  it { should have_many(:airlines).through(:flights) }
 end
