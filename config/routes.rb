f946f6a5e0f5150118c5a7ddfe1115cc6ab7bb79
Henry Schmid <henry.schmid1@gmail.com>
2021-09-24 09:14:48 -0600

    Add airline view with all passengers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :flights
-  resources :trips
+  resources :flights, only: :index
+  resources :trips, only: :destroy
+  resources :airlines, only: :show
 end
